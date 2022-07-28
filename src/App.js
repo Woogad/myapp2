bc200141b25898012c6a8fb88975d5cdbff56678
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-07-29 01:03:54 +0700

    new myapp2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
+import img1 from "./EOS-1D-X_9856-e1567152711449.png"
 function App() {
   return (
     <div>
       <header>
-        <div class="grid grid-flow-col justify-end bg-yellow-400 w-full h-12" >
-          <button class="pr-8"> หน้าหลัก </button>
-          <button class="pr-8"> หน่วยงาน </button>
-          <button class="pr-8"> ภาพกิจกรรม </button>
-          <button class="pr-8"> ปฏิทินกิจกรรม </button>
-          <button class="pr-8"> ติดต่อ </button>
-          <button class="pr-8"> Q&A/รับฟงัความคิดเห็น </button>
-          <button class="pr-8"> KUR News </button>
-          <button class="pr-8"> KRU-COVID-19 </button>
+        <div class="flex justify-end mr-36 mt-2">
+          <img src="https://www.rbac.ac.th/wp-content/uploads/2022/03/RBACCNicon2-01.png" alt="img" />
         </div>
-        <div class="grid grid-flow-col">
-          <div class=" ml-28 mt-2">
-            <img src="https://www.kru.ac.th/kru/images/header.png" />
+        <div class="grid grid-flow-col mx-10">
+          <div id="grid 1">
+            <img class="w-1/3 ml-32" src="https://www.rbac.ac.th/wp-content/uploads/2022/01/RBAC-l1-01-01.png" />
           </div>
-          <div class="grid grid-flow-col mr-28 mt-2">
-            <button>เกี่ยวกับ KRU</button>
-            <button>หลักสูตร</button>
-            <button>วิชาการ</button>
-            <button>วิจัย</button>
-            <button>วารสารวิชาการ</button>
-            <button>บริการ</button>
-            <button>E-Service</button>
+          <div id="gird 2">
+            <div id="grid 3" class="grid grid-flow-col mt-6 mr-32">
+              <button>ทำไมต้อง RBAC</button>
+              <button>คณะ/สาขา</button>
+              <button>บริการ RBAC</button>
+              <button>เกี่ยวกับ RBAC</button>
+            </div>
           </div>
         </div>
-        <div class="mt-2">
-          <img src="https://www.kru.ac.th/kru/images/slide/03.jpg" />
+        <div class="mt-6 w-full">
+          <img src={img1} alt="img1" />
         </div>
       </header>
     </div>

85fb1601fc3691a46a23ca9ef16dad2abfaf4e89
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-07-29 00:19:36 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.css 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div class="grid grid-flow-col justify-end bg-yellow-400 w-full h-12" >
+          <button class="pr-8"> หน้าหลัก </button>
+          <button class="pr-8"> หน่วยงาน </button>
+          <button class="pr-8"> ภาพกิจกรรม </button>
+          <button class="pr-8"> ปฏิทินกิจกรรม </button>
+          <button class="pr-8"> ติดต่อ </button>
+          <button class="pr-8"> Q&A/รับฟงัความคิดเห็น </button>
+          <button class="pr-8"> KUR News </button>
+          <button class="pr-8"> KRU-COVID-19 </button>
+        </div>
+        <div class="grid grid-flow-col">
+          <div class=" ml-28 mt-2">
+            <img src="https://www.kru.ac.th/kru/images/header.png" />
+          </div>
+          <div class="grid grid-flow-col mr-28 mt-2">
+            <button>เกี่ยวกับ KRU</button>
+            <button>หลักสูตร</button>
+            <button>วิชาการ</button>
+            <button>วิจัย</button>
+            <button>วารสารวิชาการ</button>
+            <button>บริการ</button>
+            <button>E-Service</button>
+          </div>
+        </div>
+        <div class="mt-2">
+          <img src="https://www.kru.ac.th/kru/images/slide/03.jpg" />
+        </div>
       </header>
     </div>
   );
